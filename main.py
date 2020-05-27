48284e3dac9c5680be0454953a49703b47a43969
simon <sipoe900@gmail.com>
2020-05-27 18:52:24 +0000

    initial working app
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import http
-
 from flask import Flask, escape, request
-
-app = Flask.name(__Meraki__)
+app = Flask(__name__)
 
 access_token = "MTc0YTFjYjgtNGM4MC00ZjFmLWJhY2QtZjUxZjIwZTEzMmE4ODljNTMyNDAtM2Qz_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
 bot_ID = "Y2lzY29zcGFyazovL3VzL0FQUExJQ0FUSU9OL2E5ZGE5ZDkwLWZjMTgtNDBiMC1iNWI3LWZmYTdlZjMxNzBmNw"
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return "Hello World!"
